a3600425e2c255015fc08ec8e2e4f502735f090f
LogunLACC <logun.johnston@lacchoa.com>
2025-07-30 15:57:27 -0700

    feat: export plain text

--- src/bulletin_builder/app_core/ui_setup.py
+++ src/bulletin_builder/app_core/ui_setup.py
@@ -77,6 +77,8 @@ def init(app):
     app.send_test_button.pack(fill="x", pady=(0,5))
     app.export_button = ctk.CTkButton(expf, text="Export to PDF...", command=app.on_export_pdf_clicked)
     app.export_button.pack(fill="x", pady=(0,5))
+    app.export_html_text_button = ctk.CTkButton(expf, text="Export HTML + Text...", command=app.on_export_html_text_clicked)
+    app.export_html_text_button.pack(fill="x", pady=(0,5))
     app.ics_button = ctk.CTkButton(expf, text="Export Event .ics", command=app.on_export_ics_clicked)
     app.ics_button.pack(fill="x")
 
